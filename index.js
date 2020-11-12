5321676e47af7135656d8fd8f4078c51dfa54263
Avick Saha <70565523+SonicBoomBoom@users.noreply.github.com>
2020-11-12 10:22:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ var started = false;
 
 var level = 0;
 
+var counterOn = false;
+
 $(document).keypress(startGame);
 
 $(".btn").click(function() {
@@ -16,22 +18,42 @@ $(".btn").click(function() {
   playSound(userChosenColour);
   animatePress(userChosenColour);
   checkAnswer(userClickedPattern.length - 1);
-  
 });
-$("#bt").click( function(){
+
+$("#bt").click(timerOn);
+
+function timer() {
   var counter = 300;
   setInterval(function() {
     counter--;
-     if (counter >= 0) {
-        span = document.getElementById("count");
-        span.innerHTML = counter;
-     }
-     if (counter === 0) {
-        alert('sorry, out of time');
-        clearInterval(counter);
-      }
-    }, 1000);
-});
+    if (counter >= 0) {
+      span = document.getElementById("count");
+      span.innerHTML = counter + " seconds";
+    }
+    if (counter === 0) {
+      alert('sorry, out of time');
+      clearInterval(counter);
+      playSound("wrong");
+      $("body").addClass("game-over");
+      $("#level-title").text("Game Over, Press 'Start' to Restart");
+      setTimeout(function() {
+        $("body").removeClass("game-over");
+      }, 200);
+      startOver();
+    }
+    if (gamePattern[userClickedPattern.length - 1] !== userClickedPattern[userClickedPattern.length - 1]) {
+      clearInterval(counter);
+      counter = "-";
+    }
+  }, 1000);
+}
+
+function timerOn() {
+  if (!counterOn) {
+    timer()
+    counterOn = true;
+  }
+}
 
 function checkAnswer(currentLevel) {
   if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
@@ -46,7 +68,7 @@ function checkAnswer(currentLevel) {
       $("body").removeClass("game-over");
     }, 200);
     startOver();
-    window.location.reload();
+    // window.location.reload();
   }
 }
 
@@ -61,6 +83,7 @@ function nextSequence() {
   userClickedPattern = [];
   level++;
   $("#level-title").text("Level " + level);
+  $("#score").text("You cleared level: " + (level - 1));
   var randomNumber = Math.floor(Math.random() * 4); // Returns a random number in a range of 0-3.
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
@@ -84,6 +107,7 @@ function startOver() {
   level = 0;
   gamePattern = [];
   started = false;
+  counterOn = false;
 }
 
 // let counter = 300000;
